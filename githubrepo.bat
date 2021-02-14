0ad48b7b0b5b184c66f7bc5e1a0295f11c2533fd
Carlos Britos <clscrea@gmail.com>
2021-02-14 12:50:21 +0100

    first commit

---
+++ githubrepo.bat
@@ -0,0 +1,7 @@
+echo "# spring-boot-backend-apirest" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/charliebp1/spring-boot-backend-apirest.git
+git push -u origin main
